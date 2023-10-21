fe9fe1447033a9c5c3ef9f32937c472a74175713
Arjunm28 <90403410+Arjunm28@users.noreply.github.com>
2023-10-20 19:26:40 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,23 @@
 import sys
 
-#Game Board
+# Game Board
 gameboard = [" " for _ in range(9)]
 
-#Players
+# Players
+X = "X"
+O = "O"
+
+#Winning combinations
+winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), 
+                        (0, 3, 6), (1, 4, 7), (2, 5, 8), 
+                        (0, 4, 8), (2, 4, 6)]
+
+#Empty Game Board
+empty_gameboard = """
+ 1 | 2 | 3
+-----------
+ 4 | 5 | 6
+-----------
+ 7 | 8 | 9
+"""
+print(empty_gameboard)
